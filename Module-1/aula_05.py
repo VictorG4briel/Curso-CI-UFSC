800f875187506a1e9b7ed479e1cf1aa74bc0dc2d
Victor <victorg2809@hotmail.com>
2025-09-16 21:24:37 -0300

    aula_06

--- Module-1/aula_05.py
+++ Module-1/aula_05.py
@@ -4,10 +4,10 @@
 #importação
 
 import numpy as np
-X=np.random.rand(5)
-W=np.random.rand(5)
+X=np.random.rand(20)
+W=np.random.rand(20)
 b=1
-beta=0.25
+beta=-0.25
 v=0
 
 
@@ -15,22 +15,22 @@ def juncaoAditiva():
     u=0
     for i in range(len(X)):
         M=W[i] 
-        if (W[i]<0.5):
-            M=W[i] + 0.5
-            print(M)
+        if (W[i]<0.6):
+            M=W[i] + 0.6
+
         N=X[i]
-        if (X[i]<0.5):
-            N=X[i] + 0.5
-        print(N)
-        v=N[i]*M[i]
+        if (X[i]<0.6):
+            N=X[i] + 0.6
+        v=N*M
         u=u+v
-        print(u)
     u=u-b
+    print(u)
     return u
 
 def main():
     z=juncaoAditiva()
     a=1*beta*z
+    print(a)
     y=1/(1+np.exp(a))
     print(f"A saida do meu neuronio é {y}")
 
