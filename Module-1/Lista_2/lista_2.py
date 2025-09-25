c6d174a8975783f2340189c78ea45bf091d9c885
Victor Gabriel <victorg2809@hotmail.com>
2025-09-24 21:36:38 -0300

    lista 2

--- Module-1/Lista_2/lista_2.py
+++ Module-1/Lista_2/lista_2.py
@@ -2,8 +2,8 @@
 num1=float(input("Digite o primeiro número: "))
 num2=float(input("Digite o segundo número: "))
 
-max=max(num1,num2)
-print(f"O maior número é: {max}")
+maximo=max(num1,num2)
+print(f"O maior número é: {maximo}")
 
 #2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
 num=float(input("Digite um número: "))
@@ -85,10 +85,9 @@ for i in range(3):
 descrescente=sorted(numeros, reverse=True)
 print(f"Números em ordem decrescente: {descrescente}")
 
-#10.numeros=[]
-for i in range(3):
-    numero=float(input(f"Digite o {i+1}º número: "))
-    numeros.append(numero)
+#10.Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+#Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso
+
 turno = input("Em que turno você estuda? (M/V/N): ").upper()
 if turno == 'M':
     print("Bom Dia!")
@@ -98,3 +97,91 @@ elif turno == 'N':
     print("Boa Noite!")
 else:
     print("Turno inválido!")
+
+#11.Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no
+#salário atual
+
+salario = float(input("Digite o valor do seu salario: "))
+salario_reajuste=0
+if (salario <=280):
+    salario_reajuste += salario * 1.2
+    valor_porcentagem = salario * 0.2
+    print(f"Salario antes do reajuste:{salario}")
+    print(f"Salario antes do reajuste:{salario_reajuste}")  
+    print("reajuste de 20%")
+    print(f"valor do aumento:{valor_porcentagem}")
+elif (280< salario <700):
+    salario_reajuste += salario * 1.15
+    valor_porcentagem = salario * 0.15
+    print(f"Salario antes do reajuste:{salario}")
+    print(f"Salario antes do reajuste:{salario_reajuste}")
+    print("reajuste de 15%")
+    print(f"valor do aumento:{valor_porcentagem}")
+
+elif (700< salario <1500):
+    salario_reajuste += salario * 1.10
+    valor_porcentagem = salario * 0.10
+    print(f"Salario antes do reajuste:{salario}")
+    print(f"Salario antes do reajuste:{salario_reajuste}")
+    print("reajuste de 10%")
+    print(f"valor do aumento:{valor_porcentagem}")
+
+else:
+    salario_reajuste+= salario* 1.05
+    valor_porcentagem = salario * 0.05
+    print(f"Salario antes do reajuste:{salario}")
+    print(f"Salario antes do reajuste:{salario_reajuste}")
+    print("reajuste de 5%")
+    print(f"valor do aumento:{valor_porcentagem}")
+
+""" 12. Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que
+depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário
+Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os
+descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+Desconto do IR:
+Salário Bruto até 900 (inclusive) - isento
+Salário Bruto até 1500 (inclusive) - desconto de 5%
+Salário Bruto até 2500 (inclusive) - desconto de 10%
+Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo
+abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220. """
+
+valor_hora = float(input("Digite o valor da sua hora de trabalho: "))
+horas_trabalhadas = float(input("Digite a quantidade de horas trabalhadas no mês:"))
+salario_bruto = valor_hora * horas_trabalhadas
+
+if (salario_bruto < 900):
+    print(f"Salario bruto({valor_hora}*{horas_trabalhadas})")
+    print("IR: isento")
+    print("Total de desconto: 0")
+    print("Total de desconto: 0")
+    print(f"Salario liquido: {salario_bruto}")
+elif (900 <salario_bruto <= 1500):
+    ir = salario_bruto * 0.05
+    inss = salario_bruto * 0.03
+    fgts= salario_bruto * 0.11
+    salario_liquido = salario_bruto - (ir - inss - fgts)
+    print(f"Salario bruto({valor_hora}*{horas_trabalhadas})")
+    print("IR: desconto de 5%")
+    print("Total de desconto: 0")
+    print("Total de desconto:")
+    print(f"Salario liquido: {salario_liquido}")
+elif (1500 <salario_bruto <= 2500):
+    ir = salario_bruto * 0.10
+    inss = salario_bruto * 0.03
+    fgts= salario_bruto * 0.11
+    salario_liquido = salario_bruto - (ir - inss - fgts)
+    print(f"Salario bruto({valor_hora}*{horas_trabalhadas})")
+    print("IR: desconto de 10%")
+    print("Total de desconto: 0")
+    print("Total de desconto:")
+    print(f"Salario liquido: {salario_liquido}")
+else:
+    ir = salario_bruto * 0.20
+    inss = salario_bruto * 0.03
+    fgts= salario_bruto * 0.11
+    salario_liquido = salario_bruto - (ir - inss - fgts)
+    print(f"Salario bruto({valor_hora}*{horas_trabalhadas})")
+    print("IR: desconto de 20%")
+    print("Total de desconto: 0")
+    print("Total de desconto:")
+    print(f"Salario liquido: {salario_liquido}")
